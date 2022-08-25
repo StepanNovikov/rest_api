2550dc7249c7c8948a262f0ede2b719c68766e32
StepanNovikov <stepan.novikov.1996@gmail.com>
2022-08-25 12:36:56 +0300

    fix message

--- pkg/logging/logging.go
+++ pkg/logging/logging.go
@@ -51,7 +51,7 @@ func init() {
 	l.Formatter = &logrus.TextFormatter{
 		CallerPrettyfier: func(frame *runtime.Frame) (function string, file string) {
 			filename := path.Base(frame.File)
-			return fmt.Sprintf("%s()", frame.Function), fmt.Sprintf("$s:$d", filename, frame.Line)
+			return fmt.Sprintf("%s()", frame.Function), fmt.Sprintf("%s:%d", filename, frame.Line)
 		},
 		DisableColors: false,
 		FullTimestamp: true,
